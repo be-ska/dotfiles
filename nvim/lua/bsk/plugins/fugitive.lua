f02cd73578c7a5b3f0c2050b3d7051d9a1821b48
Luca Pescante <luca@pescante.it>
2024-12-20 15:06:21 +0100

    fugitive: remove remap for git blame

--- nvim/lua/bsk/plugins/fugitive.lua
+++ nvim/lua/bsk/plugins/fugitive.lua
@@ -6,9 +6,6 @@ return {
         -- Open Git status
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git status" })
 
-        -- Git blame
-        vim.keymap.set("n", "<leader>gb", vim.cmd.Gblame, { desc = "Git blame" })
-
         -- Git diff split
         vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiffsplit, { desc = "Git diff split" })
     end
