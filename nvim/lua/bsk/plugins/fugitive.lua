84ebe9baacc53751befb0f2d9b5aa073cf84c7b6
Luca Pescante <luca@pescante.it>
2024-12-21 11:12:41 +0100

    nvim: open repo in GitHub with leader-g-B

--- nvim/lua/bsk/plugins/fugitive.lua
+++ nvim/lua/bsk/plugins/fugitive.lua
@@ -1,6 +1,10 @@
 -- Thanks bastard for git
 return {
     "tpope/vim-fugitive",
+    dependencies = {
+        -- use GBrowse to open GitHub repo
+        'tpope/vim-rhubarb'
+    },
     config = function()
 
         -- Open Git status
@@ -8,5 +12,8 @@ return {
 
         -- Git diff split
         vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiffsplit, { desc = "Git diff split" })
+
+        -- Browse repo in github
+        vim.keymap.set("n", "<leader>gB", vim.cmd.GBrowse, { desc = "Browse repository" })
     end
 }
