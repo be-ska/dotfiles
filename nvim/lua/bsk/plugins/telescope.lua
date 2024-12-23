e87cdede3b05eb4fe49d53b9bd4a2897fd469fcd
Luca Pescante <luca@pescante.it>
2024-12-23 18:54:13 +0100

    nvim: add Telescope warning for livegrep

--- nvim/lua/bsk/plugins/telescope.lua
+++ nvim/lua/bsk/plugins/telescope.lua
@@ -10,6 +10,7 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>tf', builtin.find_files, { desc = 'Telescope find files' })
         vim.keymap.set('n', '<leader>tt', builtin.git_files, { desc = 'Telescope find files only in repo' })
+        -- ripgrep is needed!
         vim.keymap.set('n', '<leader>ts', builtin.live_grep, { desc = 'Telescope search text in files' })
 
         require('telescope').setup({
