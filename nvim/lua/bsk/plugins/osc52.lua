79c1c0b6190f02a5fe5ba5ac728cc9d217a91667
Luca Pescante <luca@pescante.it>
2024-12-24 14:28:13 +0100

    nvim: universal keyboard even with ssh

---
+++ nvim/lua/bsk/plugins/osc52.lua
@@ -0,0 +1,26 @@
+-- global keyboard support
+return {
+    "ojroques/nvim-osc52",
+    config = function()
+        local osc = require("osc52")
+        osc.setup {
+            max_length = 0,          -- Maximum length of selection (0 for no limit)
+            silent = false,          -- Disable message on successful copy
+            trim = false,            -- Trim surrounding whitespaces before copy
+        }
+        local function copy(lines, _)
+            require('osc52').copy(table.concat(lines, '\n'))
+        end
+
+        local function paste()
+            return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
+        end
+
+        vim.g.clipboard = {
+            name = 'osc52',
+            copy = {['+'] = copy, ['*'] = copy},
+            paste = {['+'] = paste, ['*'] = paste},
+        }
+    end,
+}
+
