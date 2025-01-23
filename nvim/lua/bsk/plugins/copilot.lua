b23c7c7f49a5c1b4b92afdb78d5546a5d0c7c4de
Luca Pescante <luca@pescante.it>
2025-01-23 11:56:50 +0800

    nvim: accept copilot suggestion with right arrow

--- nvim/lua/bsk/plugins/copilot.lua
+++ nvim/lua/bsk/plugins/copilot.lua
@@ -26,7 +26,7 @@ return {
                 hide_during_completion = true,
                 debounce = 75,
                 keymap = {
-                    accept = "<Tab>",
+                    accept = "<Right>",
                     accept_word = false,
                     accept_line = false,
                     next = "<M-]>",
