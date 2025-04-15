00d9abd6e26e8a03752135c0c7f1c650b9325051
Luca Pescante <luca@pescante.it>
2025-04-15 13:03:18 +0800

    nvim: augment: add init function

--- nvim/lua/bsk/plugins/augment.lua
+++ nvim/lua/bsk/plugins/augment.lua
@@ -1,15 +1,13 @@
 -- augment AI autocomplete
 return {
     'augmentcode/augment.vim',
-    config = function()
-        local aug = require('augment')
-        
+    init = function()
+        -- Disable tab mapping BEFORE plugin loads
+        vim.g.augment_disable_tab_mapping = true
         -- fetch current folder and add to workspace
         vim.g.augment_workspace_folders = { vim.fn.getcwd() }
         -- vim.g.augment_workspace_folders = {'~/code/dotfiles'}
-
         -- Use shift Tab to accept
-        vim.g.augment_disable_tab_mapping = true
         vim.keymap.set('i', '<S-Tab>', '<cmd>call augment#Accept()<cr>')
 
         -- Send a chat message in normal and visual mode
@@ -21,6 +19,5 @@ return {
 
         -- Toggle the chat panel visibility
         vim.keymap.set('n', '<leader>at', ':Augment chat-toggle<CR>')
-
     end
 }
