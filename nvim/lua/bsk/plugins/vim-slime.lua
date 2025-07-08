e540434687b29cacf7e031270c807888f3757855
Luca Pescante <luca@pescante.it>
2025-07-08 20:56:49 +0200

    nvim: add vim-slime

---
+++ nvim/lua/bsk/plugins/vim-slime.lua
@@ -0,0 +1,8 @@
+-- send buffer to tmux
+return {
+	"jpalardy/vim-slime",
+    config = function()
+        vim.g.slime_target = "tmux"
+        vim.g.slime_default_config = { socket_name = "default", target_pane = "{last}" }
+    end
+}
