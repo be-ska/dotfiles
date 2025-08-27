c5115466a9faa16ae7c6d984fa6317686c45ce32
Luca Pescante <luca@pescante.it>
2025-08-27 16:52:05 +0200

    nvim: add ruff to mason

--- nvim/lua/bsk/plugins/lsp.lua
+++ nvim/lua/bsk/plugins/lsp.lua
@@ -14,7 +14,7 @@ return {
         config = function()
             require('mason-lspconfig').setup({
                 -- Replace the language servers listed here with the ones you want to install
-                ensure_installed = { 'clangd', 'lua_ls', 'ltex', 'lemminx', 'yamlls', 'biome', 'pyright' },
+                ensure_installed = { 'clangd', 'lua_ls', 'ltex', 'lemminx', 'yamlls', 'biome', 'pylsp', 'ruff' },
                 handlers = {
                     function(server_name)
                         require('lspconfig')[server_name].setup({})
